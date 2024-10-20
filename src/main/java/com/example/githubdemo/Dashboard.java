7c4c5375b4996341fbecae8f281807d15ce4d65e
Tashfeen21 <tashfeen.zs2000@gmail.com>
2024-10-20 10:54:18 +0600

    Added New Scene,and Dashboard

---
+++ src/main/java/com/example/githubdemo/Dashboard.java
@@ -0,0 +1,14 @@
+package com.example.githubdemo;
+
+import javafx.event.ActionEvent;
+
+public class Dashboard
+{
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void onLogOutButtonClick(ActionEvent actionEvent) {
+    }
+}
--- src/main/java/com/example/githubdemo/HelloController.java
+++ src/main/java/com/example/githubdemo/HelloController.java
@@ -1,14 +1,45 @@
 package com.example.githubdemo;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class HelloController {
     @FXML
-    private Label welcomeText;
+    private TextField passwordField;
+    @FXML
+    private TextField userName;
+    @FXML
+    private Label msgLabel;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to OOP Course");
+    public void onLoginButton(ActionEvent event) throws IOException {
+        String username = userName.getText();
+        String password = passwordField.getText();
+
+        if (username.equals("asif") && password.equals("1234")) {
+            msgLabel.setText("Log In Successfully");
+
+            FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("dashboard.fxml"));
+            Parent root = fxmlLoader.load();
+
+            Scene scene = new Scene(root);
+
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            stage.setScene(scene);
+
+
+        }
+        else {
+            msgLabel.setText("Wrong username or password");
+        }
     }
 }
