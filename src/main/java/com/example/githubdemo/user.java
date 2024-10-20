ef1f0409860e8f8cfd502383d2010129fca31087
Tashfeen21 <tashfeen.zs2000@gmail.com>
2024-10-20 09:38:42 +0600

    Update Age and Constructor

--- src/main/java/com/example/githubdemo/user.java
+++ src/main/java/com/example/githubdemo/user.java
@@ -3,4 +3,10 @@ package com.example.githubdemo;
 public class user {
     String username, password;
     int age;
+
+    public user(String username, String password, int age) {
+        this.username = username;
+        this.password = password;
+        this.age = age;
+    }
 }
