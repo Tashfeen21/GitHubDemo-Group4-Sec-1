55f22d266acd6892ff21bd1d86444d1f0e1abd58
Tashfeen21 <tashfeen.zs2000@gmail.com>
2024-10-20 09:40:55 +0600

    ekta banana add korsi

--- src/main/java/com/example/githubdemo/user.java
+++ src/main/java/com/example/githubdemo/user.java
@@ -3,10 +3,14 @@ package com.example.githubdemo;
 public class user {
     String username, password;
     int age;
+    int banana;
 
-    public user(String username, String password, int age) {
+    public user(String username, String password, int age, int banana) {
         this.username = username;
         this.password = password;
         this.age = age;
+        this.banana = banana;
+
+
     }
 }
